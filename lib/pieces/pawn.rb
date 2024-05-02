130c5a59105be15a06257c1981245782ba95f963
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-05-02 12:08:12 -0600

    feat: Add Pawn class

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,1 +1,21 @@
 # frozen_string_literal: true
+
+require_relative './pieces'
+
+# The pawn moves two steps forward and one step to the side or one step forward and two steps to the side
+class Pawn < Pieces
+  def initialize(color, curr_pos)
+    super(color, curr_pos)
+    @symbol = @color == 'white' ? "\u2659" : "\u265F"
+    @moved = false
+  end
+
+  def next_move
+    forward = @moved ? [1] : [1, 2]
+    forward.map! { |move| move * -1 } if @color == 'black'
+    forward.map do |move|
+      rank = (@curr_pos[1].to_i + move).to_s
+      @curr_pos[0] + rank if rank.between?('1', '8')
+    end.compact
+  end
+end
