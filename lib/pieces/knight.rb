bfa225adae9b80db0bfeaf67f3683a7caef7d33c
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-05-02 00:58:56 -0600

    chore: Adjust wording in #next_move of knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -13,9 +13,9 @@ class Knight < Pieces
     curr_rank = @curr_pos[1].to_i
     curr_file = @curr_pos[0].ord
 
-    [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]].map do |movement|
-      rank = (curr_rank + movement[0]).to_s
-      file = (curr_file + movement[1]).chr
+    [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]].map do |rank_move, file_move|
+      rank = (curr_rank + rank_move).to_s
+      file = (curr_file + file_move).chr
       file + rank if rank.between?('1', '8') && file.between?('a', 'h')
     end.compact
   end
