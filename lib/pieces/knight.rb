3d3974a69dacf68b2fe86514a121315b2420f318
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-05-01 21:33:11 -0600

    feat: Add Knight class

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,1 +1,22 @@
 # frozen_string_literal: true
+
+require_relative './pieces'
+
+# The knight moves two steps forward and one step to the side or one step forward and two steps to the side
+class Knight < Pieces
+  def initialize(color, curr_pos)
+    super(color, curr_pos)
+    @symbol = @color == 'white' ? "\u2658" : "\u265E"
+  end
+
+  def next_move
+    curr_rank = @curr_pos[1].to_i
+    curr_file = @curr_pos[0].ord
+
+    [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]].map do |movement|
+      rank = (curr_rank + movement[0]).to_s
+      file = (curr_file + movement[1]).chr
+      file + rank if rank.between?('1', '8') && file.between?('a', 'h')
+    end.compact
+  end
+end
