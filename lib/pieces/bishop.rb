07c1e13a53954b518fac73863e5fc4045c0f1ed3
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-05-01 23:51:43 -0600

    feat: Add Bishop class

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+
+require_relative './pieces'
+require_relative '../modules/straight_lines_move'
+
+# The bishop moves in a straight diagonal line
+class Bishop < Pieces
+  include MoveDiagonalLine
+
+  def initialize(color, curr_pos)
+    super(color, curr_pos)
+    @symbol = @color == 'white' ? "\u2657" : "\u265D"
+  end
+
+  def next_move
+    left_diagonal_line(@curr_pos) + right_diagonal_line(@curr_pos)
+  end
+
+  def squares_to(new_pos)
+    return unless next_move.include?(new_pos)
+
+    squares = in_same_left_diagonal?(@curr_pos, new_pos) ? left_diagonal_line(@curr_pos) : right_diagonal_line(@curr_pos)
+    squares.select { |square| square.between?(*[curr_pos, new_pos].sort) }.delete(new_pos)
+  end
+end
