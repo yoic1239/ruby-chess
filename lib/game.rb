c070b2f13c7a857aa5a73111b7c7bbde8355cde8
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-05-03 15:31:39 -0600

    feat: Add ChessGame class

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+# A command line Chess game where two players can play against each other.
+class ChessGame
+  def initialize
+    @board = ChessBoard.new
+    @white = []
+    @black = []
+    @curr_player = 'white'
+  end
+end
